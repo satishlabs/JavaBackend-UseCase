1ce38b7588958edea53cd681d40a92deb4a39091
satishlabs <spd61185@gmail.com>
2025-03-26 22:01:58 +0530

    Spring boot application with request token

---
+++ springboot-unique-token/src/main/java/com/satishlabs/SpringbootUniqueTokenApplication.java
@@ -0,0 +1,13 @@
+package com.satishlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUniqueTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUniqueTokenApplication.class, args);
+	}
+
+}
